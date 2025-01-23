0f5fa8ac9a85a1c73b48d8da93a609b9f7f37f09
Kabab33 <kabab33@icloud.com>
2025-01-23 21:38:18 +0100

    nvm we use .env now

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('../config.json');
+   // Require the necessary discord.js classes
+   const { Client, Events, GatewayIntentBits } = require('discord.js');
+   require('dotenv').config();
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+   // Get the token from the environment variables
+   const token = process.env.token;
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+   // Create a new client instance
+   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Log in to Discord with your client's token
-client.login(token);
+   // When the client is ready, run this code (only once).
+   // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+   // It makes some properties non-nullable.
+   client.once(Events.ClientReady, readyClient => {
+       console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+   });
+
+   // Log in to Discord with your client's token
+   client.login(token);
